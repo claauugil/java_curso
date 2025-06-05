d9e86d8a77573180d2ff8ff7c5a44dbbe561cd62
Claudia Gil <sofiagil2288@gmail.com>
2025-06-05 13:30:23 +0200

    ordeno declaraciones de variables

--- Demos/out/Out.java
+++ Demos/out/Out.java
@@ -1,8 +1,10 @@
 public class Out {
     public static void main(String[] args) {
+        
         String name = "Paula";
         String surname = "Zacarias";
         byte   age = 25;
+
         System.out.println("Hola " + name + " " + surname + " como estas a tus " + age + " años");
         System.out.printf("Hola %s %s como estas a tus %d años\n", name, surname, age);
         System.out.println("""
