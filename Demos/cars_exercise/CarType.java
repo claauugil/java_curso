5c4671b1ef65663057b450b110c4cfa88662e329
Claudia Gil <sofiagil2288@gmail.com>
2025-06-05 13:53:19 +0200

    simplifico impresión de datos

--- Demos/cars_exercise/CarType.java
+++ Demos/cars_exercise/CarType.java
@@ -6,7 +6,13 @@ public class CarType {
     boolean isAuto;
     char    type;
 
-    CarType(int numberOfSeats, String color, int numberOfDoors, String brand, boolean isAuto, char type)
+    CarType(
+            int numberOfSeats,
+            String color,
+            int numberOfDoors,
+            String brand,
+            boolean isAuto,
+            char type)
     {
         this.numberOfSeats = numberOfSeats;
         this.color = color;
@@ -14,5 +20,25 @@ public class CarType {
         this.brand = brand;
         this.isAuto = isAuto;
         this.type = type;
+    }
+    void printInfo()
+    {
+        String msg;
+        msg = """
+                *************************
+                Número de asientos: %d
+                Color: %s
+                Número de puertas: %d
+                Marca: %s
+                Es automático: %b
+                Tipo: %c
+                *************************
+                """;
+        System.out.println(msg.formatted(numberOfSeats,
+                                        color,
+                                        numberOfDoors,
+                                        brand,
+                                        isAuto,
+                                        type));
     }
 }
