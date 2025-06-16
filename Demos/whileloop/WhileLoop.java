1d0e47f629d19db3bf6da7b390b269140d924b45
Claudia Gil <sofiagil2288@gmail.com>
2025-06-16 13:03:07 +0200

    ejercicios de while

---
+++ Demos/whileloop/WhileLoop.java
@@ -0,0 +1,88 @@
+import java.util.Random;
+
+public class WhileLoop {
+
+    static void whileSample() {
+        boolean sentinel = true;
+
+        while (sentinel) {
+            Random randomCreator = new Random();
+            int number = randomCreator.nextInt(9);
+
+            if (number % 2 == 0) {
+                continue;
+            }
+
+            System.out.println("Número " + number);
+            if (number == 7) {
+                sentinel = false;
+            }
+        }
+    }
+
+    static void forSample() {
+
+        for (int i = 0; i >= 0; i++) {
+             Random randomCreator = new Random();
+            int number = randomCreator.nextInt(9);
+
+            if (number % 2 == 0) {
+                continue;
+            }
+
+            System.out.println("Número " + number);
+            if (number == 7) {
+                break;
+            }
+        }
+
+    }
+
+    static void forSample2() {
+
+        int number = 0;
+
+        for (int i = 0; number != 7 ; i++) {
+            Random randomCreator = new Random();
+            number = randomCreator.nextInt(9);
+
+            if (number % 2 == 0) {
+                continue;
+            }
+
+            System.out.printf("Número %s = %s\n", i+1, number);
+           
+        }
+
+    }
+
+        
+    static void whileSampleWithCounter() {
+        boolean sentinel = true;
+        int i = 0;
+
+        while (sentinel) {
+            i++;
+            Random randomCreator = new Random();
+            int number = randomCreator.nextInt(9);
+            
+            if (number % 2 == 0) {
+                continue;
+            }
+            
+            System.out.printf("Número %s = %s\n", i, number);
+            if (number == 7) {
+                sentinel = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // whileSample();
+        // forSample();
+        // forSample2();
+        whileSampleWithCounter();
+    }
+
+}
